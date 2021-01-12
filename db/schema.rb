6f6119cf81b3fb44b57cda136fbcae865b5b7be5
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-01-12 17:26:13 -0500

    Messages model and controller created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_12_221518) do
+ActiveRecord::Schema.define(version: 2021_01_12_222142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
