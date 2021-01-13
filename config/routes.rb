84d45be9d6859b3a560b96865dd0dfd3d268964c
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-01-12 19:56:57 -0500

    Comments model, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root to: 'messages#index'
   # root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :messages
+  resources :messages do
+    resources :comments
+  end
 end
